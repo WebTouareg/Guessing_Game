4ab6fd1c8b635f0913333c7162ddeea4d8a5a0cb
WebTouareg <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>
2025-06-09 19:20:29 +0200

    just adding comments with AI

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,11 @@
 use std::io;
 
+/*************  ✨ Command ⭐  *************/
+/// This is the main function of the program. It will print a message asking the user
+/// for a guess, read that guess from the standard input, and then print out the
+/// guess it received.
+/*******  d63a1b81-33f0-4269-b47f-6dbb34ed694c  *******/
+
 fn main () {
     println!("Guess the number");
 
