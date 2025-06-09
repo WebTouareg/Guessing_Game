78e097ac1dc5d64103edf66829e463465c9441d3
WebTouareg <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>
2025-06-09 20:03:21 +0200

    Back to original

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,29 @@
 use std::io;
 
 /*************  ✨ Command ⭐  *************/
-/// This is the main function of the program. It will print a message asking the user
-/// for a guess, read that guess from the standard input, and then print out the
-/// guess it received.
-/*******  d63a1b81-33f0-4269-b47f-6dbb34ed694c  *******/
+/// This is the main function of the program. It will print a message asking the
+/// userfor a guess, read that guess from the standard input, and then print out
+///  the guess it received.
 
-fn main () {
-    println!("Guess the number");
+// Old method for arguments before using the macro format_args! in 2022 release
+// println!("You guessed: {guess}"); vs println!("You guessed: {}", guess);
+
+//fn main () {
+//    println!("Guess the number");
+//
+//    println!("Please input your guess.");
+//
+//    let mut guess = String::new();
+//
+//    io::stdin()
+//        .read_line(&mut guess)
+//        .expect("Failed to read line");
+//
+//    println!("You guessed: {}", guess);
+//}
+
+fn main() {
+    println!("Guess the number!");
 
     println!("Please input your guess.");
 
@@ -17,22 +33,5 @@ fn main () {
         .read_line(&mut guess)
         .expect("Failed to read line");
 
-    println!("You guessed: {}", guess);
+    println!("You guessed: {guess}");
 }
-
-// use std::io;
-
-// Original code from the Rust Book
-// fn main() {
-//     println!("Guess the number!");
-//
-//     println!("Please input your guess.");
-//
-//     let mut guess = String::new();
-//
-//     io::stdin()
-//         .read_line(&mut guess)
-//         .expect("Failed to read line");
-//
-//     println!("You guessed: {guess}");
-// }
